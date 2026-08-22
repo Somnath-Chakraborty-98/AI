65db8c045f4b03e9cd4ff072e1c296210a0b01ee
Somnath-Chakraborty-98 <71351098+Somnath-Chakraborty-98@users.noreply.github.com>
2026-08-22 12:22:43 +0530

    - basics

--- PYTHON/hello.py
+++ PYTHON/hello.py
@@ -1,11 +1,16 @@
-print("Hello world")
+helloWorld = "Hello world"
+print(helloWorld)
+print(len(helloWorld))
+
 
 a = 0 
 b = 0
-myList = {1, 2, 3, 4, 5, 6}
+#dictionary
+myList = {1: "A", 2: "B", 3: "C", 4: "D", 5: "E", 6: "F"}
 n = len(myList)
 print(n)
-for x in myList:    
+for x in myList:   
+    print(myList[x]) 
     if x % 2 == 0:
         a += x
     else:
@@ -20,3 +25,21 @@ odd = b % 2 == 0
 print (even)
 print (odd)
 print(odd and even)
+
+for i in range(1, 10):
+    print(str(i))
+
+#list
+list = [1,2,"3","4"]
+for i in list:
+    print(i)
+print(list[1])
+print(list[0:2])
+
+
+#tuple
+point = (1,2,3)
+
+#set
+num = {1,2,3,4,5,5}
+print(num)
